0b0152aca18b8d3929d08916ed735aaa4373039d
Akacia <akaciapark@gmail.com>
2023-03-10 10:14:03 -0300

    fixed the cpf calculation

--- bin/cpf_calculation.dart
+++ bin/cpf_calculation.dart
@@ -1,63 +1,67 @@
-String? cpfCalculation(String cpf) {
-  final regexValidation = _regexValidation(cpf);
-  if (regexValidation != null) return regexValidation;
+class CpfNumbers {
+  final String cpf;
 
-  final handleCpf = cpf.replaceAll(RegExp(r'[\.-]'), '');
+  CpfNumbers({required this.cpf});
 
-  final firstValidation = _validateVerifierDigit(cpf: handleCpf, indexToValidate: 9);
-  if(firstValidation == false) return 'CPF inválido no primeiro dígito verificador';
+  String? cpfValidation() {
+    final regexValidation = _regexValidation();
+    if (regexValidation != null) return regexValidation;
 
-  final secondValidation = _validateVerifierDigit(cpf: handleCpf, indexToValidate: 10);
-  if(secondValidation == false) return 'CPF inválido no segundo dígito verificador';
+    final handleCpf = cpf.replaceAll(RegExp(r'[\.-]'), '');
 
-  return null;
+    final firstValidation =
+        _validateVerifierDigit(cpf: handleCpf, indexToValidate: 9);
+    if (firstValidation == false)
+      return 'CPF inválido no primeiro dígito verificador';
 
+    final secondValidation =
+        _validateVerifierDigit(cpf: handleCpf, indexToValidate: 10);
+    if (secondValidation == false)
+      return 'CPF inválido no segundo dígito verificador';
+
+    return null;
+  }
+
+  String? _regexValidation() {
+    if (cpf.trim().isEmpty) {
+      return 'Preencha o campo, ele está vazio!';
+    }
+
+    bool isValidCpf =
+        RegExp(r'^[0-9]{3,3}(\.)?[0-9]{3,3}(\.)?[0-9]{3}(\-)?[0-9]{2,2}')
+            .hasMatch(cpf);
+    if (!isValidCpf) {
+      return 'CPF inválido!';
+    }
+    return null;
+  }
+
+  bool _validateVerifierDigit(
+      {required String cpf, required int indexToValidate}) {
+    final splittedCpf = cpf.split('').map(int.parse);
+    final verifierDigit = splittedCpf.elementAt(indexToValidate);
+
+    var resultSumValidation = 0;
+
+    for (int i = 0; i <= (indexToValidate - 1); i++) {
+      final digit = splittedCpf.elementAt(i);
+      final multiplier = (indexToValidate + 1) - i;
+
+      final result = digit * multiplier;
+
+      resultSumValidation += result;
+    }
+
+    final result = resultSumValidation * 10;
+    var rest = result % 11;
+
+    if (rest == 10) {
+      rest = 0;
+    }
+
+    if (rest == verifierDigit) {
+      return true;
+    }
+    return false;
+  }
 }
-
-String? _regexValidation(String cpf) {
-  bool isValidCpf =
-      RegExp(r'^[0-9]{3,3}(\.)?[0-9]{3,3}(\.)?[0-9]{3}(\-)?[0-9]{2,2}')
-          .hasMatch(cpf);
-  if (!isValidCpf) {
-    return 'CPF inválido!';
-  }
-  return null;
-}
-
-bool _validateVerifierDigit(
-    {required String cpf, required int indexToValidate}) {
-  final splittedCpf = cpf.split('').map(int.parse);
-  final verifierDigit = splittedCpf.elementAt(indexToValidate);
-
-  var resultSumValidation = 0;
-
-  for (int i = 0; i <= (indexToValidate - 1); i++) {
-    final digit = splittedCpf.elementAt(i);
-    final multiplier = (indexToValidate + 1) - i;
-
-    final result = digit * multiplier;
-    resultSumValidation += result;
-  }
-
-  var rest = resultSumValidation % 11;
-
-  if (rest == 10) {
-    rest = 0;
-  }
-
-  if (rest == verifierDigit) {
-    return true;
-  }
-  return false;
-}
-
-// class CpfNumbers{
-//   final String 896.328.430-10;
-// }
-
-// CpfNumbers({
-//   this.
-// })
-
-
-
--- bin/semana_2.dart
+++ bin/semana_2.dart
@@ -1,3 +1,6 @@
-void main (){
-  
+import 'cpf_calculation.dart';
+
+void main() {
+  CpfNumbers cpf = CpfNumbers(cpf: '992.516.600-42');
+  print(cpf.cpfValidation());
 }
